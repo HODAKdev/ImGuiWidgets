91f004c9397e997e4ec136c99b61d66ee3d83f07
Hodak <82084934+HODAKdev@users.noreply.github.com>
2023-03-07 22:42:55 +0100

    Create Widgets.cpp

---
+++ Widgets.cpp
@@ -0,0 +1,310 @@
+   void DrawBoundingBox(const float* view, const float* projection, const float* matrix, const float* min, const float* max)
+   {
+       matrix_t viewProjection = *(matrix_t*)view * *(matrix_t*)projection;
+       vec_t frustum[6];
+       ComputeFrustumPlanes(frustum, viewProjection.m16);
+       matrix_t res = *(matrix_t*)matrix * viewProjection;
+
+       float thickness = 2.f;
+       ImU32 col = IM_COL32(0xE2, 0x52, 0x52, 0xFF);
+
+       vec_t camSpacePosition;
+       camSpacePosition.TransformPoint(makeVect(0.f, 0.f, 0.f), gContext.mMVP);
+       if (!gContext.mIsOrthographic && camSpacePosition.z < 1.5f)
+           return;
+
+       {
+           vec_t ptA = makeVect(min[0], min[1], min[2]);
+           vec_t ptB = makeVect(max[0], min[1], min[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], min[1], min[2]);
+           vec_t ptB = makeVect(min[0], max[1], min[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], min[1], min[2]);
+           vec_t ptB = makeVect(min[0], min[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], max[1], min[2]);
+           vec_t ptB = makeVect(max[0], max[1], min[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(max[0], max[1], min[2]);
+           vec_t ptB = makeVect(max[0], min[1], min[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(max[0], min[1], min[2]);
+           vec_t ptB = makeVect(max[0], min[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(max[0], max[1], min[2]);
+           vec_t ptB = makeVect(max[0], max[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], max[1], min[2]);
+           vec_t ptB = makeVect(min[0], max[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], max[1], max[2]);
+           vec_t ptB = makeVect(max[0], max[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], max[1], max[2]);
+           vec_t ptB = makeVect(min[0], min[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(min[0], min[1], max[2]);
+           vec_t ptB = makeVect(max[0], min[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(max[0], max[1], max[2]);
+           vec_t ptB = makeVect(max[0], min[1], max[2]);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+   }
+
+   void DrawPerspectiveFrustum(const float* view, const float* projection, const float* matrix, const float NearZ, const float FarZ, const float width, const float height, const float fov)
+   {
+       matrix_t viewProjection = *(matrix_t*)view * *(matrix_t*)projection;
+       vec_t frustum[6];
+       ComputeFrustumPlanes(frustum, viewProjection.m16);
+       matrix_t res = *(matrix_t*)matrix * viewProjection;
+
+       float thickness = 2.f;
+       ImU32 col = IM_COL32(0xE2, 0x52, 0x52, 0xFF);
+
+       float Hnear = 2 * tan((fov * DEG2RAD) / 2) * NearZ;
+       float Wnear = Hnear * width / height;
+
+       float Hfar = 2 * tan((fov * DEG2RAD) / 2) * FarZ;
+       float Wfar = Hfar * width / height;
+
+       {
+           vec_t ptA = makeVect(-Wnear, Hnear, NearZ);
+           vec_t ptB = makeVect(Wnear, Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wnear, Hnear, NearZ);
+           vec_t ptB = makeVect(Wnear, -Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wnear, -Hnear, NearZ);
+           vec_t ptB = makeVect(-Wnear, -Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-Wnear, -Hnear, NearZ);
+           vec_t ptB = makeVect(-Wnear, Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-Wnear, Hnear, NearZ);
+           vec_t ptB = makeVect(-Wfar, Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wnear, Hnear, NearZ);
+           vec_t ptB = makeVect(Wfar, Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wnear, -Hnear, NearZ);
+           vec_t ptB = makeVect(Wfar, -Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-Wnear, -Hnear, NearZ);
+           vec_t ptB = makeVect(-Wfar, -Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-Wfar, Hfar, FarZ);
+           vec_t ptB = makeVect(Wfar, Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wfar, Hfar, FarZ);
+           vec_t ptB = makeVect(Wfar, -Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(Wfar, -Hfar, FarZ);
+           vec_t ptB = makeVect(-Wfar, -Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-Wfar, -Hfar, FarZ);
+           vec_t ptB = makeVect(-Wfar, Hfar, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+           vec_t ptB = makeVect(-Wnear, Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+           vec_t ptB = makeVect(Wnear, Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+           vec_t ptB = makeVect(Wnear, -Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+           vec_t ptB = makeVect(-Wnear, -Hnear, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+   }
+
+   void DrawOrthographicFrustum(const float* view, const float* projection, const float* matrix, const float NearZ, const float FarZ, const float width, const float height)
+   {
+       matrix_t viewProjection = *(matrix_t*)view * *(matrix_t*)projection;
+       vec_t frustum[6];
+       ComputeFrustumPlanes(frustum, viewProjection.m16);
+       matrix_t res = *(matrix_t*)matrix * viewProjection;
+
+       float thickness = 2.f;
+       ImU32 col = IM_COL32(0xE2, 0x52, 0x52, 0xFF);
+
+       if (NearZ != 0.0f)
+       {
+           {
+               vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+               vec_t ptB = makeVect(width, height, NearZ);
+               gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+           }
+
+           {
+               vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+               vec_t ptB = makeVect(width, -height, NearZ);
+               gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+           }
+
+           {
+               vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+               vec_t ptB = makeVect(-width, -height, NearZ);
+               gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+           }
+
+           {
+               vec_t ptA = makeVect(0.0f, 0.0f, 0.0f);
+               vec_t ptB = makeVect(-width, height, NearZ);
+               gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+           }
+       }
+
+       {
+           vec_t ptA = makeVect(-width, height, NearZ);
+           vec_t ptB = makeVect(width, height, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, height, NearZ);
+           vec_t ptB = makeVect(width, -height, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, -height, NearZ);
+           vec_t ptB = makeVect(-width, -height, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-width, -height, NearZ);
+           vec_t ptB = makeVect(-width, height, NearZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-width, height, FarZ);
+           vec_t ptB = makeVect(width, height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, height, FarZ);
+           vec_t ptB = makeVect(width, -height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, -height, FarZ);
+           vec_t ptB = makeVect(-width, -height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-width, -height, FarZ);
+           vec_t ptB = makeVect(-width, height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-width, height, NearZ);
+           vec_t ptB = makeVect(-width, height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, height, NearZ);
+           vec_t ptB = makeVect(width, height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(width, -height, NearZ);
+           vec_t ptB = makeVect(width, -height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+
+       {
+           vec_t ptA = makeVect(-width, -height, NearZ);
+           vec_t ptB = makeVect(-width, -height, FarZ);
+           gContext.mDrawList->AddLine(worldToPos(ptA, res), worldToPos(ptB, res), col, thickness);
+       }
+   }
