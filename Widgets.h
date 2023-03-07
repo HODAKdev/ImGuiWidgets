bcabb959dc97975b9af23400d0de49cece02691c
Hodak <82084934+HODAKdev@users.noreply.github.com>
2023-03-07 22:46:35 +0100

    Update and rename Widgets.h to Widgets.hpp

--- Widgets.h
+++
@@ -1,3 +0,0 @@
-   IMGUI_API void DrawBoundingBox(const float* view, const float* projection, const float* matrix, const float* min, const float* max);
-   IMGUI_API void DrawPerspectiveFrustum(const float* view, const float* projection, const float* matrix, const float NearZ, const float FarZ, const float width, const float height, const float fov);
-   IMGUI_API void DrawOrthographicFrustum(const float* view, const float* projection, const float* matrix, const float NearZ, const float FarZ, const float width, const float height);
