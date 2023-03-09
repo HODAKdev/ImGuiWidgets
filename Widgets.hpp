60cccf19035a296999bc407605fb160ded1800b3
Hodak <82084934+HODAKdev@users.noreply.github.com>
2023-03-09 14:07:40 +0100

    Update Widgets.hpp

--- Widgets.hpp
+++ Widgets.hpp
@@ -1,22 +1,23 @@
-   IMGUI_API void DrawBoundingBox(const float* _View,
-                                  const float* _Projection,
-                                  const float* _Matrix,
-                                  const float* _Min,
-                                  const float* _Max);
-
-   IMGUI_API void DrawPerspectiveFrustum(const float* _View,
-                                         const float* _Projection,
-                                         const float* _Matrix,
-                                         const float _Near,
-                                         const float _Far,
-                                         const float _Width,
-                                         const float _Height,
-                                         const float _Fov);
-
-   IMGUI_API void DrawOrthographicFrustum(const float* _View,
-                                          const float* _Projection,
-                                          const float* _Matrix,
-                                          const float _Near,
-                                          const float _Far,
-                                          const float _Width,
-                                          const float _Height);
+   IMGUI_API void DrawBoundingBox(
+       const float* _View,
+       const float* _Projection,
+       const float* _Matrix,
+       const float* _Min,
+       const float* _Max);
+   IMGUI_API void DrawPerspectiveFrustum(
+       const float* _View,
+       const float* _Projection,
+       const float* _Matrix,
+       const float _Near,
+       const float _Far,
+       const float _Width,
+       const float _Height,
+       const float _Fov);
+   IMGUI_API void DrawOrthographicFrustum(
+       const float* _View,
+       const float* _Projection,
+       const float* _Matrix,
+       const float _Near,
+       const float _Far,
+       const float _Width,
+       const float _Height);
